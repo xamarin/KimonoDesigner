38529cd7b398a48a384bd6e5b504a66b4824c79b
Kevin Mullins <kevin.mullins@xamarin.com>
2017-02-13 16:43:44 -0600

    Fixed Issues with Windows Library
    Fixed issues with Windows version of the library.

---
+++ KimonoCore.Windows/MyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace KimonoCore.Windows
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
--- KimonoCore.Windows/Properties/AssemblyInfo.cs
+++ KimonoCore.Windows/Properties/AssemblyInfo.cs
@@ -1,19 +1,26 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("KimonoCore.Windows")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("KimonoCore.Windows")]
+[assembly: AssemblyCompany("Microsoft, Inc.")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Microsoft, Inc.")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f53cef1e-651b-430d-af9b-64d487cb7bd3")]
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
